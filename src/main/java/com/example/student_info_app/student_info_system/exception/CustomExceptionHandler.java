6c0c7fc90a5608bc32d5743a67ad86037196c437
spsahu <sprava198@gmail.com>
2024-11-30 15:35:06 +0530

    Added Custom exception class

---
+++ src/main/java/com/example/student_info_app/student_info_system/exception/CustomExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.example.student_info_app.student_info_system.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    //Add method for exception
+}
